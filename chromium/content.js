40ba294a4de61b4b4ae3c35341e42c6023649500
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-14 13:17:48 +0000

    Refactor article link handling to use Freedium URL.

--- chromium/content.js
+++ chromium/content.js
@@ -1,40 +1,31 @@
-const twelveFootDomain = "https://12ft.io/proxy?q=";
+const freediumUrl = "https://freedium.cfd/";
 const mediumDomain = "https://medium.com";
 
-const add12ftToMediumArticleURL = (url) => {
-  if (!url.includes(twelveFootDomain)) {
-    const urlWith12ft = twelveFootDomain + encodeURIComponent(mediumDomain + url);
-    return urlWith12ft;
-  }
-  return url;
-};
-
-const modifyEachArticleLink = () => {
-  const links = document.querySelectorAll('a[aria-label="Post Preview Title"]');
-  links.forEach((link) => {
-    const originalURL = link.getAttribute("href");
-    const modifiedURL = add12ftToMediumArticleURL(originalURL);
-    link.setAttribute("href", modifiedURL);
-
-    // Prevent the navigation to Medium's detail page, and instead
-    // navigate to the 12ft.io proxy page
-    link.addEventListener("click", (event) => {
-      event.preventDefault();
-      window.location.href = modifiedURL;
-    });
-  });
-};
-
-const observeDOMChanges = (mutationsList, observer) => {
-  for (const mutation of mutationsList) {
+function handleNewArticles(mutationsList) {
+  mutationsList.forEach((mutation) => {
     if (mutation.type === "childList") {
-      modifyEachArticleLink();
+      mutation.addedNodes.forEach((node) => {
+        if (node.tagName === "ARTICLE") {
+          const links = node.querySelectorAll('div[class^="iq"] a');
+
+          links.forEach((link) => {
+            const originalURL = link.getAttribute("href");
+            const modifiedURL = freediumUrl + originalURL;
+
+            link.setAttribute("href", modifiedURL);
+
+            link.addEventListener("click", (event) => {
+              event.preventDefault();
+              window.open(modifiedURL, "_blank");
+            });
+          });
+        }
+      });
     }
-  }
-};
+  });
+}
 
-const observer = new MutationObserver(observeDOMChanges);
-
-// Start observing the target element for changes
-const target = document.querySelector("div.dx.s");
-observer.observe(target, { childList: true, subtree: true });
+const targetNode = document.body;
+const config = { childList: true, subtree: true };
+const observer = new MutationObserver(handleNewArticles);
+observer.observe(targetNode, config);
